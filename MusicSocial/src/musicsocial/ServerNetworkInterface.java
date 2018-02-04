f7ef1f30c16f9f2c528442d5caa24101e98e3b30
Edwin <Edlangley@live.co.uk>
2018-02-04 21:44:37 +0000

    serverNetworkInterface

--- MusicSocial/src/musicsocial/ServerNetworkInterface.java
+++ MusicSocial/src/musicsocial/ServerNetworkInterface.java
@@ -1,14 +1,73 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package musicsocial;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Edwin
  */
 public class ServerNetworkInterface {
+    private Socket socket=null;
+    public boolean openConnection=true;
+    
+    
+    private void serverConnect(DataPacket data) {
+        
+        DataPacket inputData=new DataPacket();
+        DataPacket outputData=new DataPacket();
+        
+        try(
+                ObjectInputStream input = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+                ObjectOutputStream output= new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+                )
+        {
+            if (input.read()!=-1){
+                try{
+                    inputData=(DataPacket) input.readObject();
+                } catch (IOException i){
+                    i.printStackTrace();
+                    return;
+                } catch (ClassNotFoundException c){
+                    System.out.println("Class not found");
+                    c.printStackTrace();
+                    return;
+                }
+            }
+            input.close();
+            
+            if (outputData.getCommand()!=null){
+                try{
+                    output.writeObject(outputData);
+                    
+                } catch (IOException i){
+                    i.printStackTrace();
+                    return;
+                }
+            }output.close();
+            
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
     
 }
