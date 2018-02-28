a629b629e04b018fd775f88909b39c2f58efbb61
Edwin <Edlangley@live.co.uk>
2018-02-28 18:27:46 +0000

    Can add images to the database

--- SocialServer/src/Database/SQLiteJDBCDriverConnection.java
+++ SocialServer/src/Database/SQLiteJDBCDriverConnection.java
@@ -5,6 +5,11 @@
  */
 package Database;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -67,11 +72,52 @@ public void insertUser(String FirstName, String LastName, String UserName, Strin
         }
     }
     
+
+private byte[] readFile(String file) {
+        ByteArrayOutputStream bos = null;
+        try {
+            File f = new File(file);
+            FileInputStream fis = new FileInputStream(f);
+            byte[] buffer = new byte[1024];
+            bos = new ByteArrayOutputStream();
+            for (int len; (len = fis.read(buffer)) != -1;) {
+                bos.write(buffer, 0, len);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (IOException e2) {
+            System.err.println(e2.getMessage());
+        }
+        return bos != null ? bos.toByteArray() : null;
+    }
     
+
+    public void updatePicture(String UserName, String filename) {
+        // update sql
+        String updateSQL = "UPDATE Users "
+                + "SET Picture = ? "
+                + "WHERE UserName=?";
+ 
+        try (Connection conn = connect();
+                PreparedStatement pstmt = conn.prepareStatement(updateSQL)) {
+ 
+            // set parameters
+            pstmt.setBytes(1, readFile(filename));
+            pstmt.setString(2, UserName);
+ 
+            pstmt.executeUpdate();
+            System.out.println("Stored the file in the BLOB column.");
+ 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     
     public static void main(String[] args) {
         SQLiteJDBCDriverConnection app = new SQLiteJDBCDriverConnection();
         app.selectAll();
-        app.insertUser("Edwin", "Langley", "EdwinLangley1", "password", "edwin@langley.co.uk", "01010101010");
+        //app.insertUser("Edwin", "Langley", "EdwinLangley1", "password", "edwin@langley.co.uk", "01010101010");
+        app.updatePicture("EdwinLangley2", "D:\\Users\\Edwin\\Downloads\\14463110_1206091416079967_1082422483814707867_n.jpg");
+        
     }
 }
