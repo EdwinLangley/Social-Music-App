ad1e8bd95b13d87838f36f206960cca97c6cd20a
Edwin <Edlangley@live.co.uk>
2018-02-28 17:26:32 +0000

    Can now add to tables

--- SocialServer/src/Database/SQLiteJDBCDriverConnection.java
+++ SocialServer/src/Database/SQLiteJDBCDriverConnection.java
@@ -5,9 +5,12 @@
  */
 package Database;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 
 /**
@@ -17,26 +20,58 @@ import java.sql.SQLException;
 public class SQLiteJDBCDriverConnection {
     
     
-    public static void connect(){
-    
+  
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:D:/Users/Edwin/Documents/SystemsSoftware/SocialServer/db/SocialServer.db";
         Connection conn = null;
-        try{
-            
-            String url = "jdbc:sqlite:D:/Users/Edwin/Documents/SystemsSoftware/SocialServer/db/SocialServer.db";
-            
+        try {
             conn = DriverManager.getConnection(url);
-            System.out.println("Connection to SQLite has been established.");
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
-        }finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
         }
+        return conn;
+    }
     
+    
+public void selectAll(){
+        String sql = "SELECT * FROM Users";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getString("FirstName") + "," + rs.getString("LastName")+ "," + rs.getString("UserName") + "," + rs.getString("Password")+ "," + rs.getString("Email"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+public void insertUser(String FirstName, String LastName, String UserName, String Password, String Email, String blob) {
+        String sql = "INSERT INTO Users VALUES(?,?,?,?,?,?)";
+ 
+        try (Connection conn = this.connect();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, FirstName);
+            pstmt.setString(2, LastName);
+            pstmt.setString(3, UserName);
+            pstmt.setString(4, Password);
+            pstmt.setString(5, Email);
+            pstmt.setString(6, blob);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        SQLiteJDBCDriverConnection app = new SQLiteJDBCDriverConnection();
+        app.selectAll();
+        app.insertUser("Edwin", "Langley", "EdwinLangley1", "password", "edwin@langley.co.uk", "01010101010");
     }
 }
